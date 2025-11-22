81a13c464826a4a6fcacf8bf37a2e679d9659fc5
balaji-borude <balajiborude2503@gmail.com>
2025-11-22 23:13:27 +0530

    Setting up the fonts from google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono,Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const InterFont = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const spaceGroteskFont = Space_Grotesk({
+  variable: "--font-space-grotesk",
   subsets: ["latin"],
 });
+
 
 export const metadata: Metadata = {
   title: "Dev Overflow ",
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${InterFont.variable} ${spaceGroteskFont.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="text-3xl text-green-400">Hello</div>;
+  return <div >
+    Hello
+
+
+    <h1 className="font-space-grotesk text-2xl"> Test Tailwind </h1>
+    <h1 className="font-inter text-2xl">Test Tailwind</h1>
+
+    </div>;
 }
